c5552a7ffbbd2be8313ad5a2d98f8fdad13dff33
Ugitch <ugison@gmail.com>
2018-01-17 11:14:22 +0100

    Simplest version, play one round of game; add playerPlay(), playRound()

--- js/game.js
+++ js/game.js
@@ -1,10 +1,39 @@
+const beatenBy = {
+  'rock': 'paper',
+  'paper': 'scissors',
+  'scissors': 'rock'
+}
+
 function computerPlay() {
-  let randomizer = Math.floor(Math.random() * 3); 
+  let randomChoice = Math.floor(Math.random() * 3); 
   let choice = {
     0: "rock",
     1: "paper",
     2: "scissors"
   }
 
-  return choice[randomizer];
+  return choice[randomChoice];
 }
+
+function playerPlay() {
+  let playerSelection = prompt("Rock, paper, scissors?").toLowerCase();
+  return playerSelection;
+}
+
+function playRound() {
+  let computerChoice = computerPlay();
+  let playerChoice = playerPlay();
+
+  console.log(playerChoice);
+  console.log(computerChoice);
+
+  if (playerChoice === computerChoice) {
+    return "It's a tie!";
+  } else if (playerChoice === beatenBy[computerChoice]) {
+    return "Player wins!";
+  } else {
+    return "Computer wins!"
+  }
+}
+
+console.log(playRound());
