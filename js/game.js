07d44578692f75d1c111909d1054da0964079ed6
Ugitch <ugison@gmail.com>
2018-01-17 19:54:56 +0100

    Finish console version of the game

--- js/game.js
+++ js/game.js
@@ -1,8 +1,13 @@
-const beatenBy = {
+const APPROVED_CHOICES = ['rock', 'paper', 'scissors'];
+const BEATEN_BY = {
   'rock': 'paper',
   'paper': 'scissors',
   'scissors': 'rock'
 }
+
+let playerWins = 0;
+let computerWins = 0;
+let gameOver = false;
 
 function computerPlay() {
   let randomChoice = Math.floor(Math.random() * 3); 
@@ -11,29 +16,61 @@ function computerPlay() {
     1: "paper",
     2: "scissors"
   }
-
   return choice[randomChoice];
 }
 
 function playerPlay() {
   let playerSelection = prompt("Rock, paper, scissors?").toLowerCase();
-  return playerSelection;
+
+  if (APPROVED_CHOICES.indexOf(playerSelection) !== -1) {
+    return playerSelection;
+  }
+  else {
+    console.log("Wrong entry!");
+    playerSelection = playerPlay();
+    return playerSelection;
+  }
 }
 
 function playRound(computerPlay, playerPlay) {
-  let computerChoice = computerPlay();
-  let playerChoice = playerPlay();
+  const computerChoice = computerPlay();
+  const playerChoice = playerPlay();
 
   console.log(playerChoice);
   console.log(computerChoice);
 
   if (playerChoice === computerChoice) {
-    return "It's a tie!";
-  } else if (playerChoice === beatenBy[computerChoice]) {
-    return capitalizer(`${playerChoice} beats ${computerChoice}! You win!`);
-  } else {
-    return capitalizer(`${computerChoice} beats ${playerChoice}! You lose! `);
+    console.log("It's a tie!");
+  } 
+  else if (playerChoice === BEATEN_BY[computerChoice]) {
+    playerWins++;
+    console.log(capitalizer(`${playerChoice} beats ${computerChoice}! You win!`));
+  } 
+  else {
+    computerWins++
+    console.log(capitalizer(`${computerChoice} beats ${playerChoice}! You lose!`));
   }
 }
 
-console.log(playRound(computerPlay, playerPlay));
+function game() {
+  let winner = undefined;
+
+  while (!gameOver) {
+    playRound(computerPlay, playerPlay);
+
+    if (playerWins === 5) {
+      winner = "player";
+      gameOver = true;
+    } 
+    else if (computerWins === 5) {
+      winner = "computer";
+      gameOver = true;
+    } 
+    else {
+      console.log(`Current result:\n\tcomputer: ${computerWins}\n\tplayer: ${playerWins}`);
+    }
+  }
+  console.log(`The winner is ${winner}!`)
+}
+
+game();
--- js/helpers.js
+++ js/helpers.js
@@ -1,5 +1,1 @@
-// function capitalize(string) {
-//   return string[0].toUpperCase() + string.slice(1);
-// }
-
 var capitalizer = string => string[0].toUpperCase() + string.slice(1);
