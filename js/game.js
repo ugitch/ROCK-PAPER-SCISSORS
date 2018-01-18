3663e8a7cb5539c6d302af8ef4fa6c67f4804033
Ugitch <ugison@gmail.com>
2018-01-18 12:40:12 +0100

    Change name of variables and functions to preserve style consistency

--- js/game.js
+++ js/game.js
@@ -9,17 +9,17 @@ let playerWins = 0;
 let computerWins = 0;
 let gameOver = false;
 
-function computerPlay() {
-  let randomChoice = Math.floor(Math.random() * 3); 
+function letComputerPlay() {
+  let randomNumber = Math.floor(Math.random() * 3); 
   let choice = {
     0: "rock",
     1: "paper",
     2: "scissors"
   }
-  return choice[randomChoice];
+  return choice[randomNumber];
 }
 
-function playerPlay() {
+function letPlayerPlay() {
   let playerSelection = prompt("Rock, paper, scissors?").toLowerCase();
 
   if (APPROVED_CHOICES.indexOf(playerSelection) !== -1) {
@@ -27,7 +27,7 @@ function playerPlay() {
   }
   else {
     console.log("Wrong entry!");
-    playerSelection = playerPlay();
+    playerSelection = letPlayerPlay();
     return playerSelection;
   }
 }
@@ -44,19 +44,19 @@ function playRound(computerPlay, playerPlay) {
   } 
   else if (playerChoice === BEATEN_BY[computerChoice]) {
     playerWins++;
-    console.log(capitalizer(`${playerChoice} beats ${computerChoice}! You win!`));
+    console.log(capitalize(`${playerChoice} beats ${computerChoice}! You win!`));
   } 
   else {
     computerWins++
-    console.log(capitalizer(`${computerChoice} beats ${playerChoice}! You lose!`));
+    console.log(capitalize(`${computerChoice} beats ${playerChoice}! You lose!`));
   }
 }
 
-function game() {
+function playGame() {
   let winner = undefined;
 
   while (!gameOver) {
-    playRound(computerPlay, playerPlay);
+    playRound(letComputerPlay, letPlayerPlay);
 
     if (playerWins === 5) {
       winner = "player";
@@ -73,4 +73,4 @@ function game() {
   console.log(`The winner is ${winner}!`)
 }
 
-game();
+playGame();
--- js/helpers.js
+++ js/helpers.js
@@ -1,1 +1,1 @@
-var capitalizer = string => string[0].toUpperCase() + string.slice(1);
+var capitalize = string => string[0].toUpperCase() + string.slice(1);
